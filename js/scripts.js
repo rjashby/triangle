de03ba7009a417f826bb52878c0da1a6e4eca7cc
Ryan Ashby <rjapsu160@gmail.com>
2022-01-13 14:08:13 -0800

    WIP first 3 triangle work, not a triangle does not
    Co-authored by:Petia Kortcheva<peetiak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,28 @@ $(document).ready(function() {
     let side1 = $("input#side1").val();
     let side2 = $("input#side2").val();
     let side3 = $("input#side3").val();
+    $("#equilateral").hide();
+    $("#isosceles").hide();
+    $("#scalene").hide();
+    $("#not").hide();
     console.log(side1);
     console.log(side2);
     console.log(side3);
+
+    if (side1 === side2 && side2 === side3) {
+      $("#equilateral").show();
+    } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+      $("#isosceles").show();
+    } else if (side1 != side2 && side1 !== side3 && side2 !== side3) {
+      $("#scalene").show();
+    } else {
+      $("#not").show();
+    }
   });
 });
+
+// ((side1 = side2 && side1 !== side3) || (side1 = side3 && side1 !== side2) || (side2 === side3 && side2 !== side1)) {
+  // $("#isosceles").show();
+
+// } else if ((side1 + side2 >= side3) || (side1 + side3 >= side2) || (side2 + side3 >= side1)) {
+//   $("#not").show();
