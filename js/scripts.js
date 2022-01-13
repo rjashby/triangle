30b88425d74ad527a548327c2cb6632d120435f4
Ryan Ashby <rjapsu160@gmail.com>
2022-01-13 14:38:40 -0800

    BUGFIX fixed initial if statement for not a triangle
    Co-authored by:Petia Kortcheva<peetiak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $("form").submit (function(event) {
     event.preventDefault();
-    let side1 = $("input#side1").val();
-    let side2 = $("input#side2").val();
-    let side3 = $("input#side3").val();
+    let side1 = parseInt($("input#side1").val());
+    let side2 = parseInt($("input#side2").val());
+    let side3 = parseInt($("input#side3").val());
     $("#equilateral").hide();
     $("#isosceles").hide();
     $("#scalene").hide();
@@ -12,14 +12,16 @@ $(document).ready(function() {
     console.log(side2);
     console.log(side3);
 
-    if (side1 === side2 && side2 === side3) {
-      $("#equilateral").show();
-    } else if (side1 === side2 || side2 === side3 || side1 === side3) {
-      $("#isosceles").show();
-    } else if (side1 != side2 && side1 !== side3 && side2 !== side3) {
-      $("#scalene").show();
-    } else {
-      $("#not").show();
+  if ((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
+    $("#not").show();
+  } else {
+      if (side1 === side2 && side2 === side3) {
+        $("#equilateral").show();
+      } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+        $("#isosceles").show();
+      } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+        $("#scalene").show();
+      }
     }
   });
 });
